08d727871b7919b82c581e1d5a92d89891182d8f
KanameHara <opxf.8941@gmail.com>
2024-07-08 20:43:52 +0900

    マイページの投稿詳細画面で投稿データをDBから取得処理追加

--- frontend/pages/mypage/posts/[id]/show.tsx
+++ frontend/pages/mypage/posts/[id]/show.tsx
@@ -6,6 +6,7 @@ import Header from "@/components/layouts/Header";
 import { useRouter } from 'next/router';
 import React, { FC, useState } from 'react';
 import { GoogleMap, Marker, InfoWindow } from '@react-google-maps/api';
+import { PostInfoType } from "@/types/PostInfoType";
 import {
   Text,
   Flex,
@@ -39,7 +40,7 @@ const MyPagePostShow: FC = () => {
   const postID: bigint = BigInt(1);
 
   //投稿情報のサンプルなので後で削除(経度と緯度が逆になっているかも？なぜかマップ上にマーカーが表示されない)
-  const postDate = {
+  const post = {
     userID: 1,
     airport_id: "ChIJ-61QtY01QDUR4a_NVrCN6dw",
     category_id: 1,
@@ -57,6 +58,28 @@ const MyPagePostShow: FC = () => {
       "/images/sample/Boeing747.jpg",
     ]
   }
+  
+  // 未実装のためコメントアウト -->
+  // postIDに紐づく投稿情報を取得
+  // const [post, setPost] = useState<PostInfoType | null>(null);
+  // const fetchPost = async () => {
+  //   try {
+
+  //     // 指定したpostIDの投稿データを取得
+  //     const response = await axios.get(`${process.env.NEXT_PUBLIC_RAILS_SERVER_URL_DEV}/posts/${postID}`);
+  //     setPost(response.data);
+  //   } catch (error) {
+  //     console.error('Error fetching post:', error);
+  //   }
+  // };
+
+  // // 初回読み込み時にpostIDにもとづく投稿情報を取得
+  // useEffect(() => {
+  //   if (postID) {
+  //     fetchPost();
+  //   
+  // }, [postID]);
+  // 未実装のためコメントアウト <--
 
   // 現在の写真インデックスの状態管理
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -64,14 +87,14 @@ const MyPagePostShow: FC = () => {
   // 前の写真に移動
   const handlePreviousClick = () => {
     setCurrentImageIndex((prevIndex) => 
-      prevIndex === 0 ? postDate.images.length - 1 : prevIndex - 1
+      prevIndex === 0 ? post.images.length - 1 : prevIndex - 1
     );
   };
 
   // 次の写真に移動
   const handleNextClick = () => {
     setCurrentImageIndex((prevIndex) => 
-      prevIndex === postDate.images.length - 1 ? 0 : prevIndex + 1
+      prevIndex === post.images.length - 1 ? 0 : prevIndex + 1
     );
   };
 
@@ -119,10 +142,10 @@ const MyPagePostShow: FC = () => {
       </Head>
       <Header showButtonFlag={true} />
       <Box p={5} mt={10} shadow="md" borderWidth="1px" borderRadius="md" width="50%" mx="auto">
-        <Text mt={5} ml={10} fontWeight="bold">{postDate.title}</Text>
+        <Text mt={5} ml={10} fontWeight="bold">{post.title}</Text>
         <Box mt={5} ml={10} display="flex" alignItems="center">
           <Button onClick={handlePreviousClick}>&lt;</Button>
-          <Image src={postDate.images[currentImageIndex]}
+          <Image src={post.images[currentImageIndex]}
             alt={`Image ${currentImageIndex + 1}`}
             width="500px" 
             height="500px" 
@@ -134,7 +157,7 @@ const MyPagePostShow: FC = () => {
         </Box>
         <Flex>
           <Text mt={5} ml={12} fontWeight="bold">撮影日：</Text>
-          <Text mt={5} ml={5} fontWeight="bold">{postDate.taking_at}</Text>
+          <Text mt={5} ml={5} fontWeight="bold">{post.taking_at}</Text>
         </Flex>
         <Flex>
           <Text mt={5} ml={12} fontWeight="bold">空港名：</Text>
@@ -147,11 +170,11 @@ const MyPagePostShow: FC = () => {
         <div style={{ marginTop: '15px', marginBottom: '50px' }}>
           <GoogleMap
             mapContainerStyle={containerStyle}
-            center={{ lat: postDate.taking_position_latitude, lng: postDate.taking_position_longitude }}
+            center={{ lat: post.taking_position_latitude, lng: post.taking_position_longitude }}
             zoom={15}
           >
-            <Marker position={{ lat: postDate.taking_position_latitude, lng: postDate.taking_position_longitude }}>
-              <InfoWindow position={{ lat: postDate.taking_position_latitude, lng: postDate.taking_position_longitude }}>
+            <Marker position={{ lat: post.taking_position_latitude, lng: post.taking_position_longitude }}>
+              <InfoWindow position={{ lat: post.taking_position_latitude, lng: post.taking_position_longitude }}>
                 <div>撮影場所</div>
               </InfoWindow>
             </Marker>
@@ -161,7 +184,7 @@ const MyPagePostShow: FC = () => {
           <Text mt={5} ml={12} fontWeight="bold">コメント：</Text>
         </Flex>
         <Flex>
-          <Text mt={2} ml={20} fontWeight="bold">{postDate.comment}</Text>
+          <Text mt={2} ml={20} fontWeight="bold">{post.comment}</Text>
         </Flex>
         <Button mt={10} mb={50} ml={260} mr={2} colorScheme="blue" alignSelf="center"
           onClick={() => handleEditButtonClick(postID)}
