a79cab05c7f6a4e035fd502868b55f2f246373e8
KanameHara <opxf.8941@gmail.com>
2024-07-13 17:03:49 +0900

    画面表示速度アップ処理追加

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   // Googleマップ空取得した画像をImagesコンポーネントで表示するための設定
   images: {
     domains: ['maps.googleapis.com'],
+  },
+
+  // 開発環境でのパフォーマンス向上のためのwebpackDevMiddleware設定
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+
+      // ポーリングの頻度を調整してシステム負荷を軽減
+      poll: 5000,
+
+      // 再ビルドまでの待ち時間を設定
+      aggregateTimeout: 300,
+
+      // 監視から除外するパスのパターンを指定
+      ignored: ['**/node_modules/**', '**/.git/**', '**/.next/**'],
+    };
+    
+    return config;
   },
 };
 
